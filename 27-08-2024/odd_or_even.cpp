92163d5fa64bcee65fd19686cc243708d86b44fc
rohitks2004 <ksrohit25@gmail.com>
2024-08-30 12:28:30 +0530

    commit 3

--- 27-08-2024/odd_or_even.cpp
+++ 27-08-2024/odd_or_even.cpp
@@ -1,3 +1,10 @@
-String isOdd(int n) {
+#include<bits/stdc++.h>
+using namespace std;
+string isOdd(int n) {
     return ( n & 1 )? "Odd" : "Even" ;
+}
+int main(){
+    cout << isOdd(5) << endl; // Outputs: Odd
+    cout << isOdd(4) << endl; // Outputs: Even
+    return 0;
 }
